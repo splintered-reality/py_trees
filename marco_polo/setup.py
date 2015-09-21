21e3f69033b385a25ac18428f75822a036021757
Daniel Stonier <d.stonier@gmail.com>
2015-09-21 17:16:21 +0900

    [marco_polo] gopher_maps for downloading maps.

--- marco_polo/setup.py
+++ marco_polo/setup.py
@@ -7,7 +7,8 @@ d = generate_distutils_setup(
     packages=['marco_polo'],
     package_dir={'': 'src'},
     # package_data = {'rocon_uri': ['rules/rules.yaml']},
-    scripts=['scripts/gopher_teleport'],
+    scripts=['scripts/gopher_teleport',
+             'scripts/gopher_maps'],
 )
 
 setup(**d)
