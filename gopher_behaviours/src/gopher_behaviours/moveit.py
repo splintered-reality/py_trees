272aa7734a6daa2ac931079b4114bea3cb6f46b2
Michal Staniaszek <m.staniaszek@gmail.com>
2015-10-22 10:34:20 +0900

    [gopher_behaviours] fixed door behaviours to work with concert delays, door_basic allows for simple tests

--- gopher_behaviours/src/gopher_behaviours/moveit.py
+++ gopher_behaviours/src/gopher_behaviours/moveit.py
@@ -837,43 +837,74 @@ class OpenDoor(py_trees.Behaviour):
         rospy.sleep(0.2) # make sure the service client is initialised
 
     def initialise(self):
+        self.response = None
         self.open_msg_sent = False
-        self.door_opened = False
+        self.sent_status_check = False
+        self.got_initial_response = False
+        self.got_status_response = False
+
+    def err_cb(self, msg_id, result):
+        self.response = result
+    
+    def cb(self, msg_id, msg_response):
+        self.response = msg_response
 
     def update(self):
-        # send open door message
-        # wait for success
+        if self.response == None and not self.open_msg_sent:
+            req = DoorControlRequest()
+            req.stamp = rospy.Time.now()
+            req.cmd = DoorControlRequest.GET_STATUS
+            self.response = self.service_client(req, timeout=rospy.Duration(0.3))
+            self.feedback_message = "Waiting for concert to flip topics."
+            return py_trees.Status.RUNNING
+
+        rospy.loginfo("OpenDoor : Got response from concert.")
+        
         if not self.open_msg_sent:
             req = DoorControlRequest()
             req.stamp = rospy.Time.now()
             req.cmd = DoorControlRequest.OPEN_DOOR
-            resp = self.service_client(req, rospy.Duration(2))
-            if resp and resp.cmd_resp == DoorControlResponse.IGN:
+            self.response = None
+            self.service_client(req, callback=self.cb, timeout=rospy.Duration(20), error_callback=self.err_cb)
+            self.open_msg_sent = True
+
+
+        if not self.got_initial_response:
+            if self.response == None:
+                self.feedback_message = "Sent open request to door. Waiting for response."
+                return py_trees.Status.RUNNING
+            elif self.response == "timeout":
+                # request timed out
+                self.feedback_message = "Door request timed out"
+                return py_trees.Status.FAILURE
+            elif self.response and self.response.cmd_resp == DoorControlResponse.IGN:
                 # door is already open
                 self.feedback_message = "Door was already open"
                 return py_trees.Status.SUCCESS
-            self.open_msg_sent = True
-            self.feedback_message = "Sent open request to door"
-            return py_trees.Status.RUNNING
+            else:
+                self.got_initial_response = True
 
-        if not self.door_opened:
+        if not self.sent_status_check:
             req = DoorControlRequest()
             req.stamp = rospy.Time.now()
             req.cmd = DoorControlRequest.GET_STATUS
-            try:
-                resp = self.service_client(req,
-                                           timeout=rospy.Duration(0.3))
-            except rospy.ROSException as exc:
-                rospy.logwarn(self._visitor_name + " : Timed out while waiting for service server's response ("
-                              + str(exc))
+            self.response = None
+            self.service_client(req, callback=self.cb, timeout=rospy.Duration(20), error_callback=self.err_cb)
+            self.sent_status_check = True
+            
+        if not self.got_status_response:
+            if self.response == None:
+                self.feedback_message = "Sent status request to door. Waiting for response."
+                return py_trees.Status.RUNNING
+            elif self.response == "timeout":
+                # request timed out
+                self.feedback_message = "Door request timed out"
                 return py_trees.Status.FAILURE
-            except rospy.ROSInterruptException as exc:
-                rospy.logerr(self._visitor_name + " : ROS has been shut down while waiting for service server's reponse. ("
-                             + str(exc) + ").")
-                return py_trees.Status.FAILURE
-
-            if resp and resp.status == DoorControlResponse.DOOR_OPENING:
+            elif self.response and self.response.status == DoorControlResponse.DOOR_OPENING:
                 self.feedback_message = "Waiting for door to open"
+                # reset the status check and response to get the next one
+                self.sent_status_check = False
+                self.response = None
                 return py_trees.Status.RUNNING
             else:
                 self.feedback_message = "Door is now open"
@@ -886,6 +917,16 @@ class CloseDoor(py_trees.Behaviour):
         rospy.sleep(0.2) # make sure the service client is initialised
 
     def update(self):
+        if self.response == None and not self.open_msg_sent:
+            req = DoorControlRequest()
+            req.stamp = rospy.Time.now()
+            req.cmd = DoorControlRequest.GET_STATUS
+            self.response = self.service_client(req, timeout=rospy.Duration(0.3))
+            self.feedback_message = "Waiting for concert to flip topics."
+            return py_trees.Status.RUNNING
+
+        rospy.loginfo("CloseDoor : Got response from concert.")
+        
         req = DoorControlRequest()
         req.stamp = rospy.Time.now()
         req.cmd = DoorControlRequest.CLOSE_DOOR
