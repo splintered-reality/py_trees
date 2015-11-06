4ac03bd7fb3a5826add5c3c64cc75fe35f928fa8
Michal Staniaszek <m.staniaszek@gmail.com>
2015-11-06 17:58:59 +0900

    [rqt_behaviour_tree] new package for viewing behaviour trees
    Currently has an undiagnosed segfault issue. Occurs whenever the tree is
    updated, and sometimes arbitrarily. It happens sometime in the redraw_graph_view
    function. Sometimes it's a segfault, sometimes multiple free. Seems to be
    something to do with _scene.clear(). Removing that and replacing it with the
    initialisation of the scene from the init function means that things aren't
    displayed properly any more, but it no longer crashes. There is some mention of
    the QTimer only being able to be run in the QThread, and the only place that is
    used is in rqt_gui.main, as far as I can tell. Must be something to do with
    elements being drawn again when they've been deleted, but can't get in there to
    debug.

---
+++ rqt_behaviour_tree/src/rqt_behaviour_tree/behaviour_tree.py
@@ -0,0 +1,248 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2011, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import division
+import os
+
+
+import rospy
+import rospkg
+import py_trees.msg as py_trees_msgs
+
+from python_qt_binding import loadUi
+from python_qt_binding.QtCore import QFile, QIODevice, QObject, Qt, Signal
+from python_qt_binding.QtGui import QFileDialog, QGraphicsScene, QIcon, QImage, QPainter, QWidget
+from python_qt_binding.QtSvg import QSvgGenerator
+from qt_dotgraph.pydotfactory import PydotFactory
+from qt_dotgraph.dot_to_qt import DotToQtGenerator
+from rqt_graph.interactive_graphics_view import InteractiveGraphicsView
+
+from .dotcode_behaviour import RosBehaviourTreeDotcodeGenerator
+
+
+class RosBehaviourTree(QObject):
+
+    _deferred_fit_in_view = Signal()
+
+    def __init__(self, context):
+        super(RosBehaviourTree, self).__init__(context)
+        self.initialized = False
+
+        self.setObjectName('RosBehaviourTree')
+
+        self._current_dotcode = None
+
+        self._widget = QWidget()
+
+        # factory builds generic dotcode items
+        self.dotcode_factory = PydotFactory()
+        # self.dotcode_factory = PygraphvizFactory()
+        # generator builds rosgraph
+        self.dotcode_generator = RosBehaviourTreeDotcodeGenerator()
+        self.behaviour_sub = rospy.Subscriber('/gopher_deliveries/log/tree', py_trees_msgs.BehaviourTree, self.tree_cb)
+
+        # dot_to_qt transforms into Qt elements using dot layout
+        self.dot_to_qt = DotToQtGenerator()
+
+        rp = rospkg.RosPack()
+        ui_file = os.path.join(rp.get_path('rqt_behaviour_tree'), 'resource', 'RosBehaviourTree.ui')
+        loadUi(ui_file, self._widget, {'InteractiveGraphicsView': InteractiveGraphicsView})
+        self._widget.setObjectName('RosBehaviourTreeUi')
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+
+        self._scene = QGraphicsScene()
+        self._scene.setBackgroundBrush(Qt.white)
+        self._widget.graphics_view.setScene(self._scene)
+
+        self._widget.highlight_connections_check_box.toggled.connect(self._redraw_graph_view)
+        self._widget.auto_fit_graph_check_box.toggled.connect(self._redraw_graph_view)
+        self._widget.fit_in_view_push_button.setIcon(QIcon.fromTheme('zoom-original'))
+        self._widget.fit_in_view_push_button.pressed.connect(self._fit_in_view)
+        
+        self._widget.refresh_graph_push_button.setIcon(QIcon.fromTheme('view-refresh'))
+        self._widget.refresh_graph_push_button.pressed.connect(self._clear_graph)
+        
+        self._widget.load_dot_push_button.setIcon(QIcon.fromTheme('document-open'))
+        self._widget.load_dot_push_button.pressed.connect(self._load_dot)
+        self._widget.save_dot_push_button.setIcon(QIcon.fromTheme('document-save-as'))
+        self._widget.save_dot_push_button.pressed.connect(self._save_dot)
+        self._widget.save_as_svg_push_button.setIcon(QIcon.fromTheme('document-save-as'))
+        self._widget.save_as_svg_push_button.pressed.connect(self._save_svg)
+        self._widget.save_as_image_push_button.setIcon(QIcon.fromTheme('image'))
+        self._widget.save_as_image_push_button.pressed.connect(self._save_image)
+
+        self._deferred_fit_in_view.connect(self._fit_in_view,
+                                           Qt.QueuedConnection)
+        self._deferred_fit_in_view.emit()
+
+        context.add_widget(self._widget)
+
+        self._force_refresh = False
+        self.latest_message = py_trees_msgs.BehaviourTree()
+
+    def tree_cb(self, tree):
+        self.latest_message = tree
+        self._refresh_tf_graph()
+
+    def _clear_graph(self):
+        self._scene.clear()
+        self._current_dotcode = None
+
+    def save_settings(self, plugin_settings, instance_settings):
+        instance_settings.set_value('auto_fit_graph_check_box_state',
+                                    self._widget.auto_fit_graph_check_box.isChecked())
+        instance_settings.set_value('highlight_connections_check_box_state',
+                                    self._widget.highlight_connections_check_box.isChecked())
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        self._widget.auto_fit_graph_check_box.setChecked(
+            instance_settings.value('auto_fit_graph_check_box_state', True) in [True, 'true'])
+        self._widget.highlight_connections_check_box.setChecked(
+            instance_settings.value('highlight_connections_check_box_state', True) in [True, 'true'])
+        self.initialized = True
+        self._refresh_tf_graph()
+
+    def _refresh_tf_graph(self):
+        if not self.initialized:
+            return
+        self._update_graph_view(self._generate_dotcode())
+
+    def _generate_dotcode(self):
+        force_refresh = self._force_refresh
+        self._force_refresh = False
+        return self.dotcode_generator.generate_dotcode(dotcode_factory=self.dotcode_factory,
+                                                       tree=self.latest_message,
+                                                       force_refresh=force_refresh)
+
+    def _update_graph_view(self, dotcode):
+        if dotcode == self._current_dotcode:
+            return
+        self._current_dotcode = dotcode
+        self._redraw_graph_view()
+
+    def _generate_tool_tip(self, url):
+        return url
+
+    def _redraw_graph_view(self):
+        self._scene.clear()
+
+        if self._widget.highlight_connections_check_box.isChecked():
+            highlight_level = 3
+        else:
+            highlight_level = 1
+
+        (nodes, edges) = self.dot_to_qt.dotcode_to_qt_items(self._current_dotcode,
+                                                            highlight_level)
+
+        for node_item in nodes.itervalues():
+            self._scene.addItem(node_item)
+        for edge_items in edges.itervalues():
+            for edge_item in edge_items:
+                edge_item.add_to_scene(self._scene)
+
+        self._scene.setSceneRect(self._scene.itemsBoundingRect())
+        if self._widget.auto_fit_graph_check_box.isChecked():
+            self._fit_in_view()
+
+    def _load_dot(self, file_name=None):
+        if file_name is None:
+            file_name, _ = QFileDialog.getOpenFileName(
+                self._widget,
+                self.tr('Open graph from file'),
+                None,
+                self.tr('DOT graph (*.dot)'))
+            if file_name is None or file_name == '':
+                return
+
+        try:
+            fhandle = open(file_name, 'rb')
+            dotcode = fhandle.read()
+            fhandle.close()
+        except IOError:
+            return
+
+        self._update_graph_view(dotcode)
+
+    def _fit_in_view(self):
+        self._widget.graphics_view.fitInView(self._scene.itemsBoundingRect(),
+                                             Qt.KeepAspectRatio)
+
+    def _save_dot(self):
+        file_name, _ = QFileDialog.getSaveFileName(self._widget,
+                                                   self.tr('Save as DOT'),
+                                                   'frames.dot',
+                                                   self.tr('DOT graph (*.dot)'))
+        if file_name is None or file_name == '':
+            return
+
+        file = QFile(file_name)
+        if not file.open(QIODevice.WriteOnly | QIODevice.Text):
+            return
+
+        file.write(self._current_dotcode)
+        file.close()
+
+    def _save_svg(self):
+        file_name, _ = QFileDialog.getSaveFileName(
+            self._widget,
+            self.tr('Save as SVG'),
+            'frames.svg',
+            self.tr('Scalable Vector Graphic (*.svg)'))
+        if file_name is None or file_name == '':
+            return
+
+        generator = QSvgGenerator()
+        generator.setFileName(file_name)
+        generator.setSize((self._scene.sceneRect().size() * 2.0).toSize())
+
+        painter = QPainter(generator)
+        painter.setRenderHint(QPainter.Antialiasing)
+        self._scene.render(painter)
+        painter.end()
+
+    def _save_image(self):
+        file_name, _ = QFileDialog.getSaveFileName(
+            self._widget,
+            self.tr('Save as image'),
+            'frames.png',
+            self.tr('Image (*.bmp *.jpg *.png *.tiff)'))
+        if file_name is None or file_name == '':
+            return
+
+        img = QImage((self._scene.sceneRect().size() * 2.0).toSize(),
+                     QImage.Format_ARGB32_Premultiplied)
+        painter = QPainter(img)
+        painter.setRenderHint(QPainter.Antialiasing)
+        self._scene.render(painter)
+        painter.end()
+        img.save(file_name)
---
+++ rqt_behaviour_tree/src/rqt_behaviour_tree/dotcode_behaviour.py
@@ -0,0 +1,166 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import with_statement, print_function
+
+import time
+import rospy
+import yaml
+import uuid_msgs.msg as uuid_msgs
+import py_trees.msg as py_trees_msgs
+
+class RosBehaviourTreeDotcodeGenerator(object):
+
+    def __init__(self):
+        self.last_drawargs = None
+        self.dotcode = None
+        self.firstcall = True
+        self.rank = None
+        self.rankdir = None
+        self.ranksep = None
+        self.graph = None
+        self.dotcode_factory = None
+
+    def generate_dotcode(self,
+                         dotcode_factory,
+                         timer=rospy.Time,
+                         tree=None,
+                         rank='same',   # None, same, min, max, source, sink
+                         ranksep=0.2,   # vertical distance between layers
+                         rankdir='TB',  # direction of layout (TB top > bottom, LR left > right)
+                         force_refresh=False):
+        """
+        :param force_refresh: if False, may return same dotcode as last time
+        """
+        if self.firstcall is True:
+            self.firstcall = False
+            force_refresh = True
+
+        drawing_args = {
+            'dotcode_factory': dotcode_factory,
+            "rank": rank,
+            "rankdir": rankdir,
+            "ranksep": ranksep}
+
+        selection_changed = False
+        if self.last_drawargs != drawing_args:
+            selection_changed = True
+            self.last_drawargs = drawing_args
+
+            self.dotcode_factory = dotcode_factory
+            self.rank = rank
+            self.rankdir = rankdir
+            self.ranksep = ranksep
+
+        self.graph = self.generate(tree.behaviours, tree.header.stamp)
+        self.dotcode = self.dotcode_factory.create_dot(self.graph)
+
+        return self.dotcode
+
+    def type_to_shape(self, behaviour_type):
+        if behaviour_type == py_trees_msgs.Behaviour.BEHAVIOUR:
+            return 'box'
+        elif behaviour_type == py_trees_msgs.Behaviour.SEQUENCE:
+            return 'box'
+        elif behaviour_type == py_trees_msgs.Behaviour.SELECTOR:
+            return 'ellipse'
+        else:
+            return 'box'
+
+    def type_to_colour(self, behaviour_type):
+        if behaviour_type == py_trees_msgs.Behaviour.BEHAVIOUR:
+            return None
+        elif behaviour_type == py_trees_msgs.Behaviour.SEQUENCE:
+            return '#ff9900'
+        elif behaviour_type == py_trees_msgs.Behaviour.SELECTOR:
+            return '#808080'
+        else:
+            return None
+
+    def status_to_colour(self, behaviour_status):
+        if behaviour_status == py_trees_msgs.Behaviour.INVALID:
+            return '#e4e4e4'
+        elif behaviour_status == py_trees_msgs.Behaviour.RUNNING:
+            return '#000000'
+        elif behaviour_status == py_trees_msgs.Behaviour.FAILURE:
+            return '#ff0000'
+        elif behaviour_status == py_trees_msgs.Behaviour.SUCCESS:
+            return '#00ff00'
+        else:
+            return None
+        
+    def generate(self, data, timestamp):
+        graph = self.dotcode_factory.get_graph(rank=self.rank,
+                                               rankdir=self.rankdir,
+                                               ranksep=self.ranksep)
+
+        if len(data) == 0:
+            self.dotcode_factory.add_node_to_graph(graph, 'No behaviour data received')
+            return graph
+
+        root = uuid_msgs.UniqueID() # the root has an empty UUID
+        for behaviour in data:
+            self.dotcode_factory.add_node_to_graph(graph,
+                                                   str(behaviour.own_id),
+                                                   nodelabel=behaviour.name,
+                                                   shape=self.type_to_shape(behaviour.type),
+                                                   color=self.status_to_colour(behaviour.status) or self.type_to_colour(behaviour.type))
+            for child_id in behaviour.child_ids:
+                self.dotcode_factory.add_edge_to_graph(graph, 
+                                                       str(behaviour.own_id),
+                                                       str(child_id))
+
+        #     if not tf_frame_values['parent'] in data:
+        #         root = tf_frame_values['parent']
+        #     self.dotcode_factory.add_node_to_graph(graph,
+        #                                            tf_frame_values['parent'],
+        #                                            shape='ellipse')
+
+        #     edge_label= '"Broadcaster: %s\\n' % str(tf_frame_values['broadcaster'])
+        #     edge_label += 'Average rate: %s\\n' % str(tf_frame_values['rate'])
+        #     edge_label += 'Buffer length: %s\\n' % str(tf_frame_values['buffer_length'])
+        #     edge_label += 'Most recent transform: %s\\n' % str(tf_frame_values['most_recent_transform'])
+        #     edge_label += 'Oldest transform: %s"' % str(tf_frame_values['oldest_transform'])
+        #     self.dotcode_factory.add_edge_to_graph(graph,
+        #                                            tf_frame_values['parent'],
+        #                                            frame_dict,
+        #                                            label=edge_label)
+
+        # # create legend before root node
+        # legend_label = '"Recorded at time: %s"' % str(timestamp)
+        # self.dotcode_factory.add_node_to_graph(graph, legend_label)
+        # self.dotcode_factory.add_edge_to_graph(graph,
+        #                                        legend_label,
+        #                                        root,
+        #                                        style='invis')
+
+        return graph
