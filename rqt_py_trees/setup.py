48c2ec8e6ff4056d7f62ac6741f9c38cbcb5ba23
Daniel Stonier <d.stonier@gmail.com>
2016-02-11 21:03:44 +0900

    [rqt_py_trees] setup install rules for python sub-packages.

--- rqt_py_trees/setup.py
+++ rqt_py_trees/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_py_trees'],
+    packages=['rqt_py_trees', 'rqt_py_trees.plugins'],
     package_dir={'': 'src'},
     scripts=['scripts/rqt_py_trees']
 )
