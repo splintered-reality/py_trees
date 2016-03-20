f8f1b09a19e81b7f5931008b9449351128eb408c
Daniel Stonier <d.stonier@gmail.com>
2016-03-21 08:37:12 +0900

    [gopher_behaviours] explain the setup_ros functions.

--- gopher_behaviours/src/gopher_behaviours/navigation.py
+++ gopher_behaviours/src/gopher_behaviours/navigation.py
@@ -66,7 +66,14 @@ class SimpleMotion(py_trees.Behaviour):
 
     def setup_ros(self, timeout):
         """
-        Allow a higher level pastafarian to wait for the server to come up.
+        Wait for the action server to come up. Note that ordinarily you do not
+        need to call this directly since the :py:function:initialise::`initialise`
+        method will call this for you on the first entry into the behaviour, but it
+        does so with an insignificant timeout. This however is relying on the
+        assumption that the underlying system is up and running before the
+        behaviour is started, so this method provides a means for a higher level
+        pastafarian to wait for the components to fall into place first.
+
         :param double timeout: time to wait (0.0 is blocking forver)
         :returns: whether it timed out waiting for the server or not.
         :rtype: boolean
@@ -158,7 +165,14 @@ class MoveIt(py_trees.Behaviour):
 
     def setup_ros(self, timeout):
         """
-        Allow a higher level pastafarian to wait for the server to come up.
+        Wait for the action server to come up. Note that ordinarily you do not
+        need to call this directly since the :py:function:initialise::`initialise`
+        method will call this for you on the first entry into the behaviour, but it
+        does so with an insignificant timeout. This however is relying on the
+        assumption that the underlying system is up and running before the
+        behaviour is started, so this method provides a means for a higher level
+        pastafarian to wait for the components to fall into place first.
+
         :param double timeout: time to wait (0.0 is blocking forver)
         :returns: whether it timed out waiting for the server or not.
         :rtype: boolean
