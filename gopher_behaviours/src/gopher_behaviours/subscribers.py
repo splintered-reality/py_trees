6d487869becf1bd42aba90c05c13e8905aef5eb0
Daniel Stonier <d.stonier@gmail.com>
2016-03-21 11:19:31 +0900

    [gopher_behaviours] a more general base subscriber handler.

--- gopher_behaviours/src/gopher_behaviours/subscribers.py
+++ gopher_behaviours/src/gopher_behaviours/subscribers.py
@@ -34,6 +34,8 @@ class CheckSubscriberVariable(py_trees.Behaviour):
     """
     Check a subscriber to see if it has a specific variable
     and optionally whether that variable has a specific value.
+
+    .. todo:: base this off the subscriber handler.
     """
     def __init__(self,
                  name="Check Subscriber Variable",
@@ -75,6 +77,15 @@ class CheckSubscriberVariable(py_trees.Behaviour):
             self._setup()
 
     def stop(self, new_status=py_trees.Status.INVALID):
+        """
+        Cleanup if we're not tasked with monitoring continuously.
+
+        .. todo::
+
+           It might be that we may wish to reset variables on stop, even if monitoring
+           continously. If that turns out to be a use case, we need to implement
+           something for that.
+        """
         if not self.monitor_continuously:
             with self.data_guard:
                 self.cached_status = new_status
@@ -130,3 +141,61 @@ class CheckSubscriberVariable(py_trees.Behaviour):
             self.feedback_message = copy.copy(self.cached_feedback)
             status = copy.copy(self.cached_status)
         return status
+
+
+class SubscriberHandler(py_trees.Behaviour):
+    """
+    Not intended for direct use, as this just absorbs the mechanics of setting up
+    a subscriber for use by children who inherit and implement their own custom
+    update function.
+    """
+    def __init__(self,
+                 name="Subscriber Handler",
+                 topic_name="/foo",
+                 topic_type=None,
+                 monitor_continuously=False
+                 ):
+        """
+        :param str name: name of the behaviour
+        :param str topic_name: name of the topic to connect to
+        :param obj topic_type: class of the message type (e.g. std_msgs.String)
+        :param bool monitor_continuously: setup subscriber immediately and continuously monitor the data
+        """
+        super(CheckSubscriberVariable, self).__init__(name)
+        self.topic_name = topic_name
+        self.topic_type = topic_type
+        self.msg = None
+        self.subscriber = None
+        self.data_guard = threading.Lock()
+        self.monitor_continuously = monitor_continuously
+        if self.monitor_continuously:
+            self._setup()
+
+    def initialise(self):
+        if not self.monitor_continuously:
+            self._setup()
+
+    def stop(self, new_status=py_trees.Status.INVALID):
+        if not self.monitor_continuously:
+            with self.data_guard:
+                self.msg = None
+                if self.subscriber is not None:
+                    self.subscriber.unregister()
+                    self.subscriber = None
+
+    def _setup(self):
+        """
+        This will get called early if it was flagged in the constructor. This is useful
+        if you want to continuously monitor and just reflect the result as needed.
+        The converse case is when you only want to start monitoring as soon as the behaviour
+        begins running (i.e. on initialise).
+        """
+        with self.data_guard:
+            self.subscriber = rospy.Subscriber(self.topic_name, self.topic_type, self._callback)
+
+    def _callback(self, msg):
+        """
+        Simple stores the message for children of this class to implement a custom update function.
+        """
+        with self.data_guard:
+            self.msg = msg
