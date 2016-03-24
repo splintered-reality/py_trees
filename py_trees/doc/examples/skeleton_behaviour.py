222b765262eb5e6165028456ff6e3e212bfa66f4
Daniel Stonier <d.stonier@gmail.com>
2016-03-24 15:46:24 +0900

    [py_trees] get ready for py_trees exceptions!

--- py_trees/doc/examples/skeleton_behaviour.py
+++ py_trees/doc/examples/skeleton_behaviour.py
@@ -10,7 +10,6 @@ class Foo(py_trees.Behaviour):
         function so we can generate dot graphs offline
         """
         super(Foo, self).__init__(name)
-        self.foobar = True
 
     def setup(self, timeout):
         """
@@ -51,8 +50,11 @@ class Foo(py_trees.Behaviour):
           - return a py_trees.Status.[RUNNING, SUCCESS, FAILURE]
         """
         self.logger.debug("  %s [Foo::update()]" % self.name)
-        foobar = random.choice([True, False])
-        if foobar:
+        ready_to_make_a_decision = random.choice([True, False])
+        decision = random.choice([True, False])
+        if not ready_to_make_a_decision:
+            return py_trees.Status.RUNNING
+        elif decision:
             self.feedback_message = "We are not bar!"
             return py_trees.Status.SUCCESS
         else:
@@ -66,4 +68,4 @@ class Foo(py_trees.Behaviour):
             - SUCCESS || FAILURE : your behaviour's work cycle has finished
             - INVALID : a higher priority branch has interrupted, or shutting down
         """
-        self.logger.debug("  %s [Foo::terminate()]" % self.name)
+        self.logger.debug("  %s [Foo::terminate().terminate()][%s->%s]" % (self.name, self.status, new_status))
