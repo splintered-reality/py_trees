d6efd16d5b151ea453bb0085245a38ff0b82ff72
Daniel Stonier <d.stonier@gmail.com>
2016-03-25 02:23:58 +0900

    [py_trees] import all submodules to help pydev indexing and provide core as conveniences only

--- py_trees/src/py_trees/__init__.py
+++ py_trees/src/py_trees/__init__.py
@@ -17,16 +17,21 @@ package. It provides ...
 # Imports
 ##############################################################################
 
+from . import behaviour
+from . import behaviours
+from . import blackboard
+from . import composites
+from . import display
+from . import meta
+from . import rostime
+from . import subscribers
+from . import trees
+
+# conveniences
 from .behaviour import Behaviour
-
-import meta        # factories and operators on behaviours
-import behaviours  # specific behaviours
-
 from .blackboard import Blackboard, CheckBlackboardVariable
 from .common import Status
 from .composites import Composite, Selector, Sequence, OneshotSequence
 from .subscribers import CheckSubscriberVariable, SubscriberToBlackboard
 from .rostime import Pause, Timeout
 from .trees import BehaviourTree, ROSBehaviourTree, CONTINUOUS_TICK_TOCK, VisitorBase
-
-import display
