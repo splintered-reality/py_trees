4ca51422ee4a73b486ecfd753d196368925f0b74
Daniel Stonier <d.stonier@gmail.com>
2016-05-17 11:42:30 +0900

    [rqt_py_trees] bugfix missing setup.py rule for qt_dotgraph submodule.

--- rqt_py_trees/setup.py
+++ rqt_py_trees/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_py_trees', 'rqt_py_trees.plugins'],
+    packages=['rqt_py_trees', 'rqt_py_trees.plugins', 'rqt_py_trees.qt_dotgraph'],
     package_dir={'': 'src'},
     scripts=['scripts/rqt_py_trees']
 )
