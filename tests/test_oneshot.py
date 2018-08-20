f9f57f928f2a73606bb8b05374f2a9b27f62e883
Daniel Stonier <d.stonier@gmail.com>
2018-08-20 15:06:08 -0400

    [meta] remove superflous arguments to tick_tree in test_oneshot.py

--- tests/test_oneshot.py
+++ tests/test_oneshot.py
@@ -27,13 +27,13 @@ logger = py_trees.logging.Logger("Nosetest")
 ##############################################################################
 
 
-def tick_tree(root, from_iteration, to_iteration):
+def tick_tree(root):
     """
     Tick the tree and return the node count on the last tick.
     @returns count : the number of nodes traversed on the last tick
     """
-    print("\n================== Iteration %s-%s ==================\n" % (from_iteration, to_iteration))
-    for i in range(from_iteration, to_iteration + 1):
+    print("\n================== Iteration 1-2 ==================\n")
+    for i in range(1, 3):
         count = 0
         print(("\n--------- Run %s ---------\n" % i))
         for unused_node in root.tick():
@@ -56,17 +56,17 @@ def test_oneshot_does_not_modify_class():
     print(console.bold + "****************************************************************************************\n" + console.reset)
     oneshot = py_trees.meta.oneshot(py_trees.composites.Sequence)(name="Oneshot",
                                                                   children=[py_trees.behaviours.Failure(name="Failure")])
-    oneshot_count = tick_tree(oneshot, 1, 2)
+    oneshot_count = tick_tree(oneshot)
     print(console.yellow + "\nOneshot Sequence Tick Count: {}".format(oneshot_count) + console.reset)
 
     decorated_oneshot = OneShotSequence(name="Oneshot",
                                         children=[py_trees.behaviours.Failure(name="Failure")])
-    decorated_oneshot_count = tick_tree(decorated_oneshot, 1, 2)
+    decorated_oneshot_count = tick_tree(decorated_oneshot)
     print(console.yellow + "\nDecorated Oneshot Sequence Tick Count: {}".format(decorated_oneshot_count) + console.reset)
 
     normal = py_trees.composites.Sequence(name="Normal",
                                           children=[py_trees.behaviours.Failure(name="Failure")])
-    normal_count = tick_tree(normal, 1, 2)
+    normal_count = tick_tree(normal)
     print(console.yellow + "\nNormal Sequence Tick Count: {}".format(normal_count) + console.reset)
     assert(oneshot_count == 1)
     # Hitherto, the decorator modified the sequence permanently, so the tick count for the latter
