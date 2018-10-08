cdd7d165af6c147a29c280546bee0db07d3b19cf
Daniel Stonier <d.stonier@gmail.com>
2018-10-08 10:31:40 -0400

    [idoms] oneshot skeleton

--- py_trees/idioms.py
+++ py_trees/idioms.py
@@ -19,6 +19,7 @@ representing common behaviour tree idioms.
 from .common import Status
 from .behaviour import Behaviour
 
+from . import blackboard
 from . import composites
 # from . import meta
 
@@ -26,5 +27,29 @@ from . import composites
 # Creational Methods
 ##############################################################################
 
-def pick_up_where_you_left_off():
+def create_pick_up_where_you_left_off_subtree():
     pass
+
+def create_oneshot_subtree(name, variable_name, behaviour):
+    """
+    """
+    # TODO smart check to see if it's a list of behavoiurs or just a single
+    # behaviour.
+    subtree_root = composites.Selector(name=name)
+    check_flag = blackboard.CheckBlackboardVariable(
+        name="Done?",
+        variable_name=variable_name,
+        expected_value=True,
+        clearing_policy=py_trees.common.ClearingPolicy.NEVER
+    )
+    set_flag = blackboard.SetBlackboardVariable(
+        name="Mark Done",
+        variable_name=variable_name,
+        variable_value=True
+    )
+
+    sequence = composites.Sequence(name="OneShot")
+    sequence.add_children([behaviour, set_flag])
+
+    subtree_root.add_children([check_flag, sequence])
+    return subtree_root
