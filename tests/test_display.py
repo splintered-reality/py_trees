e8c940185ea168c49bae5e6fb33ca35329c5bbf6
Daniel Stonier <d.stonier@gmail.com>
2019-04-22 23:26:28 -0400

    more tests

--- tests/test_display.py
+++ tests/test_display.py
@@ -15,6 +15,22 @@ import py_trees.display as display
 import xml.etree.ElementTree
 
 ##############################################################################
+# Helpers
+##############################################################################
+
+
+def assert_banner():
+    print(console.green + "----- Asserts -----" + console.reset)
+
+
+def assert_details(text, expected, result):
+    print(console.green + text +
+          "." * (40 - len(text)) +
+          console.cyan + "{}".format(expected) +
+          console.yellow + " [{}]".format(result) +
+          console.reset)
+
+##############################################################################
 # Tests
 ##############################################################################
 
@@ -50,49 +66,55 @@ def test_symbols():
 def test_xhtml_tree():
     console.banner("Ascii/Html Snapshots - Comparison Check")
 
-    def print_tree(snapshot_visitor, tree):
-        # Visited
-        print(
-            py_trees.display.ascii_tree(
-                tree.root,
-                visited=snapshot_visitor.visited,
-                previously_visited=snapshot_visitor.previously_visited
-            )
-        )
-        html_snippet = py_trees.display.xhtml_tree(
+    root = py_trees.composites.Selector("Selector")
+    high_priority = py_trees.behaviours.Count(
+        name="High Priority",
+        fail_until=1,
+        running_until=1,
+        success_until=10
+    )
+    low_priority = py_trees.behaviours.Running(name="Low Priority")
+    root.add_children([high_priority, low_priority])
+    tree = py_trees.trees.BehaviourTree(root)
+    snapshot_visitor = py_trees.visitors.SnapshotVisitor()
+    tree.visitors.append(snapshot_visitor)
+    tree.tick()
+    tree.tick()
+    snippets = {}
+    # Visited
+    snippets["visited_ascii_tree"] = py_trees.display.ascii_tree(
             tree.root,
             visited=snapshot_visitor.visited,
             previously_visited=snapshot_visitor.previously_visited
         )
-        print(html_snippet)
-        print()
-        # Non-Visited
-        print(py_trees.display.ascii_tree(tree.root))
-        print()
-        non_visited_html_snippet = py_trees.display.xhtml_tree(tree.root)
-        print(non_visited_html_snippet)
-        print()
-        try:
-            unused_element = xml.etree.ElementTree.fromstring(html_snippet)
-        except xml.etree.ElementTree.ParseError:
-            assert False, "failed to parse the xhtml snippet as valid xml"
-
-    root = py_trees.composites.Selector("Selector")
-    root.add_child(
-        py_trees.behaviours.Count(
-            name="High Priority",
-            fail_until=1,
-            running_until=1,
-            success_until=10
-        )
+    print(snippets["visited_ascii_tree"])
+    snippets["visited_xhtml"] = py_trees.display.xhtml_tree(
+        tree.root,
+        visited=snapshot_visitor.visited,
+        previously_visited=snapshot_visitor.previously_visited
     )
-    root.add_child(py_trees.behaviours.Running(name="Low Priority"))
-    tree = py_trees.trees.BehaviourTree(root)
-    snapshot_visitor = py_trees.visitors.SnapshotVisitor()
-    tree.visitors.append(snapshot_visitor)
-    tree.add_post_tick_handler(functools.partial(print_tree, snapshot_visitor))
-    tree.tick()
-    tree.tick()
+    print(snippets["visited_xhtml"])
+    print()
+    # Non-Visited
+    snippets["non_visited_ascii_tree"] = py_trees.display.ascii_tree(tree.root)
+    print(snippets["non_visited_ascii_tree"])
+    print()
+    snippets["non_visited_xhtml"] = py_trees.display.xhtml_tree(tree.root)
+    print(snippets["non_visited_xhtml"])
+    print()
+
+    assert_banner()
+    for snippet_name, snippet in snippets.items():
+        for b in root.iterate():
+            assert_details("{} in {}".format(b.name, snippet_name), True, b.name in snippet)
+            assert(b.name in snippet)
+    try:
+        unused_element = xml.etree.ElementTree.fromstring(snippets["visited_xhtml"])
+        unused_element = xml.etree.ElementTree.fromstring(snippets["non_visited_xhtml"])
+        assert_details("xml ParseError", None, None)
+    except xml.etree.ElementTree.ParseError as e:
+        assert_details("xml ParseError", None, str(e))
+        assert False, "failed to parse the xhtml snippet as valid xml"
 
 
 def test_ascii_snapshot_priority_interrupt():
