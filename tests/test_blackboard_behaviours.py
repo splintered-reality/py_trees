cfaa64c9599a11f2b901ec7df05c28d95b17edbc
Daniel Stonier <d.stonier@gmail.com>
2019-10-15 16:14:10 -0400

    [blackboard] unregister keys (#241)

--- tests/test_blackboard_behaviours.py
+++ tests/test_blackboard_behaviours.py
@@ -340,7 +340,7 @@ def test_wait_for_variable_value():
         assert(b.status == asserted_result)
 
 
-def untest_wait_for_blackboard_variable():
+def test_wait_for_blackboard_variable():
     console.banner("Wait for Blackboard Variable")
     create_blackboard()
 
@@ -353,18 +353,18 @@ def untest_wait_for_blackboard_variable():
         name="check_nested_foo_exists", variable_name="nested.foo"), Status.SUCCESS))
     tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
         name="check_nested_bar_exists", variable_name="nested.bar"), Status.RUNNING))
-    tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
+    tuples.append((py_trees.behaviours.WaitForBlackboardVariableValue(
         name="check_foo_equals_bar", variable_name="foo", expected_value="bar"), Status.SUCCESS))
-    tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
+    tuples.append((py_trees.behaviours.WaitForBlackboardVariableValue(
         name="check_foo_equals_foo", variable_name="foo", expected_value="foo"), Status.RUNNING))
-    tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
+    tuples.append((py_trees.behaviours.WaitForBlackboardVariableValue(
         name="check_bar_equals_bar", variable_name="bar", expected_value="bar"), Status.RUNNING))
-    tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
+    tuples.append((py_trees.behaviours.WaitForBlackboardVariableValue(
         name="check_bar_equals_foo", variable_name="bar", expected_value="foo"), Status.RUNNING))
-    tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
+    tuples.append((py_trees.behaviours.WaitForBlackboardVariableValue(
         name="check_nested_foo_equals_bar", variable_name="nested.foo", expected_value="bar"),
         Status.SUCCESS))
-    tuples.append((py_trees.behaviours.WaitForBlackboardVariable(
+    tuples.append((py_trees.behaviours.WaitForBlackboardVariableValue(
         name="check_nested_foo_equals_foo", variable_name="nested.foo", expected_value="foo"),
         Status.RUNNING))
     for b, unused in tuples:
