3984a173a1fba0f77a08f8feb1dcc7a96a07eccc
Daniel Stonier <d.stonier@gmail.com>
2020-07-20 19:44:36 -0400

    [infra] simplified venv (#294)

--- venv.bash
+++ venv.bash
@@ -3,7 +3,9 @@
 # Script for setting up the development environment.
 #source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
-NAME=py_trees
+PROJECT=py_trees
+SRC_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+VENV_DIR=${SRC_DIR}/.venv
 
 ##############################################################################
 # Colours
@@ -68,37 +70,59 @@ install_package ()
 
 ##############################################################################
 
-install_package virtualenvwrapper || return
-install_package kcachegrind || return
+#############################
+# Checks
+#############################
 
-# To use the installed python3
-VERSION="--python=/usr/bin/python3"
-# To use a specific version
-# VERSION="--python=python3.6"
-
-if [ "${VIRTUAL_ENV}" == "" ]; then
-  workon ${NAME}
-  result=$?
-  if [ $result -eq 1 ]; then
-    mkvirtualenv ${VERSION} ${NAME}
-  fi
-  if [ $result -eq 127 ]; then
-    pretty_error "Failed to find virtualenvwrapper aliases: 1) re-log or 2) source virtualenvwrapper.sh in your shell's .rc"
-    return 1
-  fi
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && SOURCED=1
+if [ -z "$SOURCED" ]; then
+  pretty_error "This script needs to be sourced, i.e. source './setup.bash', not './setup.bash'"
+  exit 1
 fi
+
+#############################
+# System Dependencies
+#############################
+
+pretty_header "System Dependencies"
+install_package python3-dev || return
+install_package python3-venv || return
+
+#############################
+# Virtual Env
+#############################
+
+pretty_header "Virtual Environment"
+
+if [ -x ${VENV_DIR}/bin/pip3 ]; then
+    pretty_print "  $(padded_message "virtual_environment" "found [${VENV_DIR}]")"
+else
+    python3 -m venv ${VENV_DIR}
+    pretty_warning "  $(padded_message "virtual_environment" "created [${VENV_DIR}]")"
+fi
+
+source ${VENV_DIR}/bin/activate
+
+#############################
+# Pypi Dependencies
+#############################
+
+pretty_header "PyPi Dependencies"
+
+# build environment depedencies
+pip3 install wheel
+pip3 install "setuptools==45.2"
 
 # Get all dependencies for testing, doc generation
 
 # pip install -e .[docs]
 # we have to restrict versions because of bleeding edge incompatibilities
-pip install -r rtd-requirements.txt
-
-pip install -e .[test]
-pip install -e .[debs]
+pip3 install -r rtd-requirements.txt
+pip3 install -e .[test]
+pip3 install -e .[debs]
 
 # NB: this automagically nabs install_requires
-python setup.py develop
+python3 setup.py develop
 
 echo ""
 echo "Leave the virtual environment with 'deactivate'"
