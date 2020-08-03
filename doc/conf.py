22037871257a51b6d433d8c4618236f6be65be19
Daniel Stonier <d.stonier@gmail.com>
2020-08-02 21:45:32 -0400

    0.7.2

--- doc/conf.py
+++ doc/conf.py
@@ -82,7 +82,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'py_trees'
-copyright = u'2017, Daniel Stonier'
+copyright = u'2020, Daniel Stonier'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
