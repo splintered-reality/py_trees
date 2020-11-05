67cf8bf39096540bc24612b46cc84ddc6b1ebe8a
Sara Sinback <sara.sinback@gmail.com>
2020-11-05 12:57:24 -0500

    [tests] add test demonstrating broken static multi-element Sequences

--- tests/test_sequences.py
+++ tests/test_sequences.py
@@ -10,6 +10,7 @@
 
 import py_trees
 import py_trees.console as console
+import typing
 
 ##############################################################################
 # Logging Level
@@ -37,6 +38,52 @@ def assert_details(text, expected, result):
 ##############################################################################
 # Tests
 ##############################################################################
+
+
+def test_static_sequence_successes():
+    console.banner('Static Sequence Successes')
+    assert_banner()
+
+    success1 = py_trees.behaviours.Success(name="Success1")
+    success2 = py_trees.behaviours.Success(name="Success2")
+    success3 = py_trees.behaviours.Success(name="Success3")
+    success4 = py_trees.behaviours.Success(name="Success4")
+
+    children = [
+        success1,
+        success2,
+        success3,
+        success4,
+    ]
+    root = py_trees.composites.Sequence(name="Sequence", children=children)
+
+    root.tick_once()
+
+    assert_details("Current Child", success4.name, root.current_child.name)
+    assert root.tip().name == success4.name, 'should execute all 4 nodes of this Sequence'
+
+
+def test_static_sequence_with_failure():
+    console.banner('Static Sequence With Failure')
+    assert_banner()
+
+    success1 = py_trees.behaviours.Success(name="Success1")
+    success2 = py_trees.behaviours.Success(name="Success2")
+    failure = py_trees.behaviours.Failure(name="Failure")
+    success3 = py_trees.behaviours.Success(name="Success3")
+
+    children = [
+        success1,
+        success2,
+        failure,
+        success3,
+    ]
+    root = py_trees.composites.Sequence(name="Sequence", children=children)
+
+    root.tick_once()
+
+    assert_details("Current Child", failure.name, root.current_child.name)
+    assert root.tip().name == failure.name, 'should execute first 2 nodes of this Sequence and fail'
 
 
 def test_tick_add_with_current_child():
