d40e6264f0987909785946544fd0468b31cb7364
Daniel Stonier <d.stonier@gmail.com>
2020-12-11 10:48:44 -0500

    2.1.3

--- py_trees/version.py
+++ py_trees/version.py
@@ -16,4 +16,4 @@ setup.py and sphinx.
 ##############################################################################
 
 # When changing, Also update setup.py and package.xml
-__version__ = '2.1.2'
+__version__ = '2.1.3'
