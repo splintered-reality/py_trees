636946e017781683d879323bdc02bffab4a191cc
Sebastian Castro <sebas.a.castro@gmail.com>
2024-12-03 20:23:41 -0500

    Add trivial unittest test to fix CI

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+import importlib
+import unittest
+
+class ImportTest(unittest.TestCase):
+
+    def test_import(self):
+        """
+        This test serves to make the buildfarm happy in Python 3.12 and later.
+        See https://github.com/colcon/colcon-core/issues/678 for more information.
+        """
+        assert importlib.util.find_spec("py_trees")
