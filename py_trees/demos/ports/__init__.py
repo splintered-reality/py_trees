d41dde9bc6024cb82243431674ebc751daab41bf
Jennifer Buehler <jennifer.e.buehler@gmail.com>
2026-05-18 23:15:30 +0200

    clean up __init__

--- py_trees/demos/ports/__init__.py
+++ py_trees/demos/ports/__init__.py
@@ -7,14 +7,3 @@
 ##############################################################################
 
 """Demo scripts for the experimental :mod:`py_trees.ports` module."""
-
-##############################################################################
-# Imports
-##############################################################################
-
-# fmt: off
-from . import basic  # usort:skip  # noqa: F401
-from . import nested_subtrees  # usort:skip  # noqa: F401
-from . import remapping  # usort:skip  # noqa: F401
-from . import xml_tree  # usort:skip  # noqa: F401
-# fmt: on
