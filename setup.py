05549414f1df332f41564fac3183f54d4a8e27cb
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2026-07-14 16:52:24 -0400

    Change license to BSD-3-Clause in setup.py
    Updated license type from 'BSD' to 'BSD-3-Clause'.

--- setup.py
+++ setup.py
@@ -32,14 +32,13 @@ d = setup(
     classifiers=[
         "Environment :: Console",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development :: Libraries",
     ],
     description="pythonic implementation of behaviour trees",
     long_description="A behaviour tree implementation for rapid development of small scale decision making engines that don't need to be real time reactive.",
-    license="BSD",
+    license="BSD-3-Clause",
     entry_points={
         "console_scripts": [
             "py-trees-render = py_trees.programs.render:main",
